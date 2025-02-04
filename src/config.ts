c118e146eeb47cf44c43e7832ae7756c0af27ab0
twx <133498461+twxxxx@users.noreply.github.com>
2025-02-04 21:20:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy' // 代理码密钥
 // 代理
 export let PROXYS: Proxy[] = [ 
   {
-        domain: "google.00fa.me",
+        domain: "googlev.twx.best",
         url: "https://www.google.com/",
         enable: true
     }
