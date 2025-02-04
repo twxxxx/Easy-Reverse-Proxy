4db2a7d0d68cf95fe6ef0f63fa8115786c44aa0c
twx <133498461+twxxxx@users.noreply.github.com>
2025-02-04 21:11:32 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
-import type {Proxy} from './util/model';
+import {Proxy} from './util/model';
 
-export let PORT = 377; // 端口
+export let PORT = 370; // 端口
 
-export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
+export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy' // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXYS: Proxy[] = [ 
+  {
+        domain: "google.00fa.me",
+        url: "https://www.google.com/",
+        enable: true
+    }
+];
